b6283c9dd816cfc18639a15271ea3ea70079730e
Alexander Soen <alexander.soen@anu.edu.au>
2020-05-11 13:20:41 +1000

    Add example algorithm

---
+++ example_path_algorithm.py
@@ -0,0 +1,14 @@
+#%%
+from abstract_path_algorithm import AbstractPathAlgorithm
+
+class ExamplePathAlgorithm(AbstractPathAlgorithm):
+
+    def __init__(self):
+        super().__init__()
+
+        self.visited = self.visited  # Updated automatically
+	self.goal = self.goal        # Fixed for you to work towards
+
+    def find_move(self, available_pos):
+        # For you to do
+        return None # You need to return an element of `available_pos`
